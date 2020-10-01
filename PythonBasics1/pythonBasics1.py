8197d6d9e8a37f03c60d3e1fd07bded86d4a7450
Troy Tillery <ttiller4@uncc.edu>
2020-10-01 13:34:20 -0400

    Activity 3.0 intial commit

---
+++ PythonBasics1/pythonBasics1.py
@@ -0,0 +1,33 @@
+# Python Activtiy
+# 
+# Fill in the code for the functions below. 
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+
+# Part A. starts_with
+# Define a function starts_with(s, char) that takes a string and a character
+# and returns true if the string starts with that character and false otherwise. 
+def starts_with(s, char):
+  # YOUR CODE HERE
+  
+  return
+
+# Part B. starts_with_vowel
+# Define a function starts_with_vowel(s) that takes a string and
+# returns true if the string starts with a vowel and false otherwise. 
+# For our purposes, a consonant is any letter other than A, E, I, O, U)
+# Your solution should work for both upper and lower cases 
+def starts_with_vowel(s):
+  # YOUR CODE HERE
+  
+  return
+
+# Part C. max_min_sum
+# Define a function max_min_sum(arr) that takes an array and returns the sum
+# of the largest element and the smallest element of the array.
+# For an empty array it should return zero
+# For an array with just one element, it should return that element
+def max_min_sum(arr):
+  # YOUR CODE HERE
+  
+  return
---
+++ PythonBasics1/pythonBasics1Test.py
@@ -0,0 +1,61 @@
+
+import pythonBasics1
+# main() is already set up to call the functions 
+# we want to test with a few different inputs,
+# printing 'OK' when each function is correct.
+# the simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+  
+# Calls the functions in pythonBasics1 with interesting inputs.
+def main():
+    # set which functions to test
+    check_starts_with = True
+    check_starts_with_vowel = True
+    check_max_min_sum = True
+
+    if check_starts_with:
+        print('starts_with')
+        test(pythonBasics1.starts_with('vest','v'), True)
+        test(pythonBasics1.starts_with('crypt','c'), True)
+        test(pythonBasics1.starts_with('#python','#'), True)
+        test(pythonBasics1.starts_with('hello','H'), False)
+        test(pythonBasics1.starts_with('',' '), False)
+        test(pythonBasics1.starts_with('1',''), False)
+        test(pythonBasics1.starts_with('',''), True)
+        test(pythonBasics1.starts_with(' ',' '), True)
+    
+    if check_starts_with_vowel:
+        print()
+        print('starts_with_vowel')
+        test(pythonBasics1.starts_with_vowel('vest'), False)
+        test(pythonBasics1.starts_with_vowel('east'), True)
+        test(pythonBasics1.starts_with_vowel('Eat'), True)
+        test(pythonBasics1.starts_with_vowel('Ice'), True)
+        test(pythonBasics1.starts_with_vowel('in'), True)
+        test(pythonBasics1.starts_with_vowel('at'), True)
+        test(pythonBasics1.starts_with_vowel('All'), True)
+        test(pythonBasics1.starts_with_vowel('on'), True)
+        test(pythonBasics1.starts_with_vowel('Out'), True)
+        test(pythonBasics1.starts_with_vowel('unlock'), True)
+        test(pythonBasics1.starts_with_vowel('Unique'), True)
+        test(pythonBasics1.starts_with_vowel(''), False)
+
+  
+    if check_max_min_sum:
+        print()
+        print('max_min_sum')
+        test(pythonBasics1.max_min_sum([1,2,3,4,5]),6)
+        test(pythonBasics1.max_min_sum([3,2,5,4,6]),8)
+        test(pythonBasics1.max_min_sum([-1,0,-3,-99,-2]),-99)
+        test(pythonBasics1.max_min_sum([6]),6)
+        test(pythonBasics1.max_min_sum([]),0)
+
+if __name__ == '__main__':
+  main()
