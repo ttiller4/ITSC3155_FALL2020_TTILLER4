def210f8cc50511884c56e5b423315170c54b7a0
troytillery <ttiller4@uncc.edu>
2020-10-13 18:07:34 +0000

    Activity 5.0 initial commit

---
+++ PythonBasics3/pythonBasics3.py
@@ -0,0 +1,39 @@
+# Python Activtiy
+#
+# Fill in the code for the functions below.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+
+# import regular expression module
+import re
+
+# # Part A. starts_with_number
+# Define a function starts_with_number(s) that takes a string and returns true
+# if it starts with a number and false otherwise.
+# (For our purposes, a number is any character that is 0,1,2,3,4,5,6,7,8, or 9.)
+# Note: Be sure to use RegEx!
+def starts_with_number(s):
+  # YOUR CODE HERE
+
+  return
+
+# # Part B. starts_with_consonant
+# Define a function starts_with_consonant(s) that takes a string and returns true
+# if it starts with a consonant and false otherwise.
+# (For our purposes, a consonant is any letter other than A, E, I, O, U.)
+# Note: Be sure to use RegEx and it works for both upper and lower case and for nonletters!
+def starts_with_consonant(s):
+  # YOUR CODE HERE
+
+  return
+
+
+# Part C. binary_multiple_of_4
+# define a method binary_multiple_of_4(s) that takes a string and returns true
+# if the string represents a binary number that is a multiple of 4.
+# Note: Be sure it returns false if the string is not a valid binary number!
+# Hint: Use regular expressions to match for the pattern of a binary number that is a multiple of 4.
+def binary_multiple_of_4(s):
+  # YOUR CODE HERE
+
+  return
---
+++ PythonBasics3/pythonBasics3Test.py
@@ -0,0 +1,72 @@
+
+import pythonBasics3
+# main() is already set up to call the functions
+# we want to test with a few different inputs,
+# printing 'OK' when each function is correct.
+# the simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+# Calls the functions in pythonBasics3 with interesting inputs.
+def main():
+    # set which functions to test
+    starts_with_number = True
+    starts_with_consonant = False
+    binary_multiple_of_4 = False
+
+    if starts_with_number:
+        print('starts_with_number')
+        test(pythonBasics3.starts_with_number('12'), True)
+        test(pythonBasics3.starts_with_number('21'), True)
+        test(pythonBasics3.starts_with_number('31'), True)
+        test(pythonBasics3.starts_with_number('41'), True)
+        test(pythonBasics3.starts_with_number('51'), True)
+        test(pythonBasics3.starts_with_number('61'), True)
+        test(pythonBasics3.starts_with_number('71'), True)
+        test(pythonBasics3.starts_with_number('81'), True)
+        test(pythonBasics3.starts_with_number('91'), True)
+        test(pythonBasics3.starts_with_number('Aadfdf'), False)
+        test(pythonBasics3.starts_with_number('#foo'), False)
+        test(pythonBasics3.starts_with_number(''), False)
+
+    if starts_with_consonant:
+        print('starts_with_consonant')
+        test(pythonBasics3.starts_with_consonant('vest'), True)
+        test(pythonBasics3.starts_with_consonant('cast'), True)
+        test(pythonBasics3.starts_with_consonant('cast'), True)
+        test(pythonBasics3.starts_with_consonant('cast'), True)
+        test(pythonBasics3.starts_with_consonant('#3'), False)
+        test(pythonBasics3.starts_with_consonant('east'), False)
+        test(pythonBasics3.starts_with_consonant('Eat'), False)
+        test(pythonBasics3.starts_with_consonant('Ice'), False)
+        test(pythonBasics3.starts_with_consonant('in'), False)
+        test(pythonBasics3.starts_with_consonant('at'), False)
+        test(pythonBasics3.starts_with_consonant('All'), False)
+        test(pythonBasics3.starts_with_consonant('on'), False)
+        test(pythonBasics3.starts_with_consonant('Out'), False)
+        test(pythonBasics3.starts_with_consonant('unlock'), False)
+        test(pythonBasics3.starts_with_consonant('Unique'), False)
+        test(pythonBasics3.starts_with_consonant(''), False)
+
+    if binary_multiple_of_4:
+        print()
+        print('binary_multiple_of_4')
+        testset1 = ["1010101010100", "0101010101010100", "100", "0"]
+
+        for case in testset1:
+            test(pythonBasics3.binary_multiple_of_4(case),True)
+
+        testset2 = ["101", "1000000000001", "1001", "a100", ""]
+
+        for case in testset2:
+            test(pythonBasics3.binary_multiple_of_4(case),False)
+
+
+if __name__ == '__main__':
+  main()
