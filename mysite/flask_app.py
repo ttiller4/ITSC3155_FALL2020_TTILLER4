ea1fd04751d285b3847853aa347168e6d6c395ba
troytillery <ttiller4@uncc.edu>
2020-10-27 19:07:38 +0000

    flask app changed url to index

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,11 +1,14 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/index')
 def hello_world():
     return 'Welcome, Notes App User!'
+
+
+
 
