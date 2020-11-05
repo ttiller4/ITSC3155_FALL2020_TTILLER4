fbcf9e97e3fc4d15f5d6b0a98a8635b6400d6cf3
troytillery <ttiller4@uncc.edu>
2020-11-05 19:51:02 +0000

    flask app #3 - added navigation links

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -5,6 +5,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route('/index')
+def index():
+    a_user = {'name': 'Mogli', 'email':'mogli@uncc.edu'}
+
+    return render_template('index.html', user = a_user)
+
+@app.route('/notes')
+def get_notes():
+        notes = {1: {'title': 'First Note', 'text': 'This is my first note', 'date': '10-1-2020'},
+             2: {'title': 'Second Note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title': 'Third Note', 'text': 'This is my third note', 'date': '10-3-2020'}
+             }
+        return render_template('notes.html', notes=notes)
+
 @app.route('/notes/<note_id>')
 def get_note(note_id):
 
